9f344b8ac5da58d78fbea8fcaacdd850829ab4e4
kpark <kpark@wayfair.com>
2019-04-21 18:57:23 -0400

    initial drag functionality done

--- src/Sticker.js
+++ src/Sticker.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 class Sticker extends Component {
+
+  constructor(props) {
+    super(props);
+    this.stickerRef = React.createRef();
+  }
 
 	static propTypes = {
 		initialPosition: PropTypes.shape({
@@ -16,43 +22,80 @@ class Sticker extends Component {
 
 	state = {
 		position: this.props.initialPosition,
-		dragging: false
+		dragging: false,
+    rel: null // mouse position relative to the component x, y
 	}
 
   componentDidUpdate(prevProps, prevState) {
     if (this.state.dragging && !prevState.dragging) {
-      document.addEventListener('mousemove', this.onMouseMove)
-      document.addEventListener('mouseup', this.onMouseUp)
+      document.addEventListener('mousemove', this.onMouseMove);
+      document.addEventListener('mouseup', this.onMouseUp);
     } else if (!this.state.dragging && prevState.dragging) {
-      document.removeEventListener('mousemove', this.onMouseMove)
-      document.removeEventListener('mouseup', this.onMouseUp)
+      document.removeEventListener('mousemove', this.onMouseMove);
+      document.removeEventListener('mouseup', this.onMouseUp);
     }
   }
 
+  onMouseDown = (e) => {
+    console.log('mouseDown')
+
+    // allow left mouse button only
+    if (e.button !== 0) return;
+
+    const stickerPosition = this.stickerRef.current.getBoundingClientRect();
+
+    this.setState({
+      dragging: true,
+      rel: {
+        x: e.pageX - stickerPosition.left, // mouse position x - sticker position x
+        y: e.pageY - stickerPosition.top  // mouse position y - sticker position y
+      }
+    });
+    
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  onMouseUp = (e) => {
+    console.log('mouseUp')
+    this.setState({dragging: false});
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
   onMouseMove = (e) => {
-  // 	if (!this.state.dragging) return;
 
-  	console.log(e)
+    console.log('mouseMove')
 
-	 // 	this.setState({
-  //     position: {
-  //       x: e.pageX - this.state.rel.x,
-  //       y: e.pageY - this.state.rel.y
-  //     }
-  //   })
-  //   e.stopPropagation();
-  //   e.preventDefault();
+  	if (!this.state.dragging) return;
+
+	 	this.setState({
+      position: {
+        x: e.pageX - this.state.rel.x,  // setting sticker position x
+        y: e.pageY - this.state.rel.y  // setting sticker position y
+      }
+    });
+
+    e.stopPropagation();
+    e.preventDefault();
   }
 
   render() {
-  	console.log("state =\n")
-  	console.log(this.state)
+   //  console.log(this.state.dragging)
+  	// console.log(this.state.position)
 
     return (
-      <div 
+      <div
+        ref={this.stickerRef}
+        // draggable
       	className="Sticker"
-      	style={{width:50, height:50, backgroundColor: 'red'}}
-      	onMouseMove={this.onMouseMove}
+      	style={{width:50, 
+                height:50, 
+                backgroundColor: 'red', 
+                position: 'absolute', 
+                left: this.state.position.x + 'px', 
+                top: this.state.position.y + 'px'}}
+        onMouseDown={this.onMouseDown}
       >
       </div>
     );
